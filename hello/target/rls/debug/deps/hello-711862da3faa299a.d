f0bf275db93cff56fc5b0616b79784d526b2fd2d
user <user@rodrigo.rlc>
2020-08-15 13:55:49 -0300

    ok

---
+++ hello/target/rls/debug/deps/hello-711862da3faa299a.d
@@ -0,0 +1,5 @@
+/home/user/Documentos/Rust-lang/rustLanguage/hello/target/rls/debug/deps/hello-711862da3faa299a.rmeta: src/main.rs
+
+/home/user/Documentos/Rust-lang/rustLanguage/hello/target/rls/debug/deps/hello-711862da3faa299a.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/rls/debug/deps/hello-fe3eac1d31bd6eb0.d
@@ -0,0 +1,5 @@
+/home/user/Documentos/Rust-lang/rustLanguage/hello/target/rls/debug/deps/hello-fe3eac1d31bd6eb0.rmeta: src/main.rs
+
+/home/user/Documentos/Rust-lang/rustLanguage/hello/target/rls/debug/deps/hello-fe3eac1d31bd6eb0.d: src/main.rs
+
+src/main.rs:
